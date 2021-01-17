b36ae3dd2800020f2b8c17f948e772fc1a6bec5a
Girija Nandan Kar <42700980+girijakar@users.noreply.github.com>
2021-01-17 17:02:40 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,20 @@
+<?php
+
+   if(isset($_POST['submit']))
+   {
+   
+       $name=$_POST['name'];
+       $email=$_POST['email'];
+       $mssg=$_POST['message'];
+       $subject=$_POST['subject'];
+   
+       
+       $to='b170087@nitsikkim.ac.in';
+   
+       $subject=$subject;
+   
+       $message="Name".$name."\n"."Email:".$email."\n"."Message:".$mssg.
+
+       $header="From:".$email;
+   }
+?>
